c41da79ffbb59c6cec8db8f544b5780e2738d569
kartikmoradiya <kartikmoradiya5964@gmail.com>
2024-03-15 19:04:20 +0530

    one

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ const busRoute = require('./busRoutes')
 const routeRoute = require('./routeRoutes')
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000; 
 
 app.use('/bus',busRoute);
 app.use('/r',routeRoute);
 
-app.listen(4500);
-// git push
-
-// Comment added
+app.listen(PORT);
