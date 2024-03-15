eb7535195c5a3bb74733913890d219de1ca6c83d
kartikmoradiya <kartikmoradiya5964@gmail.com>
2024-03-15 18:01:57 +0530

    one

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,8 @@
-re('./db');
-// const router = express.Router();
-// const busRoute = require('./busRoutes')
-// const routeRoute = require('./routeRoutes')
-// const bodyParser = require('body-parser'); 
-// app.use(bodyParser.json());
-// app.use('/',busRoute);
-// app.use('/',rout
+/db');
+const router = express.Router();
+const busRoute = require('./busRoutes')
+const routeRoute = require('./routeRoutes')
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json());
+app.use('/',busRoute);
+app.use('/',route
