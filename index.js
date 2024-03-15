43db8538e2ea63a0aa26aa5238f7af60ce5f56d2
kartikmoradiya <kartikmoradiya5964@gmail.com>
2024-03-15 16:51:43 +0530

    Third

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express();
-const db = require('./db');
-const router = express.Router();
-const busRoute = require('./busRoutes')
-const routeRoute = require('./routeRoutes')
-const bodyParser = require('body-parser'); 
-app.use(bodyParser.json());
-app.use('/bus',busRoute);
-app.use('/route',routeRoute);
+// const db = require('./db');
+// const router = express.Router();
+// const busRoute = require('./busRoutes')
+// const routeRoute = require('./routeRoutes')
+// const bodyParser = require('body-parser'); 
+// app.use(bodyParser.json());
+// app.use('/bus',busRoute);
+// app.use('/route',routeRoute);
 
 app.get("/",(req,resp)=>{
     return resp.send("Welcome")
 })
-app.listen(4500);
+// app.listen(4500);
 // git push
 
 // Comment added
