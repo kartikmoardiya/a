82b44bf8b3f81cd1c65760ebbe6c241e2af8e3b1
kartikmoradiya <kartikmoradiya5964@gmail.com>
2024-03-15 19:12:27 +0530

    one

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const express = require('express')
 const app = express();
-
-app.use("/", (res,resp)=>{
-    resp.json({
-        msg : "msg"
-    })
-})
-
 const db = require('./db');
 const router = express.Router();
 const busRoute = require('./busRoutes')
@@ -14,6 +7,13 @@ const routeRoute = require('./routeRoutes')
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000; 
+
+app.use("/", (res,resp)=>{
+    resp.json({
+        msg : "msg"
+    })
+})
+
 
 app.use('/bus',busRoute);
 app.use('/r',routeRoute);
