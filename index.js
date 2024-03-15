9d1fcf053e64acc03f660f9f52435f622cedad45
kartikmoradiya <kartikmoradiya5964@gmail.com>
2024-03-15 18:07:02 +0530

    one

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express();
-// const db = require('./db');
-// const router = express.Router();
-// const busRoute = require('./busRoutes')
-// const routeRoute = require('./routeRoutes')
-// const bodyParser = require('body-parser'); 
-// app.use(bodyParser.json());
-// app.use('/',busRoute);
-// app.use('/',routeRoute);
+const db = require('./db');
+const router = express.Router();
+const busRoute = require('./busRoutes')
+const routeRoute = require('./routeRoutes')
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json());
+app.use('/b',busRoute);
+app.use('/a',routeRoute);
 
 app.use("/", (res,resp)=>{
     resp.json({
